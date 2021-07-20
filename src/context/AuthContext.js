68ac6e67eb5829a844b39885b3fb3139ff2eaab8
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-20 23:33:25 +0300

    feat: Add handling error request, add AsyncStorage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,26 +1,33 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
-      return { ...state, errorMessage: action.payload, token: "" };
+      return { ...state, errorMessage: action.payload };
+    case "signup":
+      return { ...state, errorMessage: "", token: action.payload };
     default:
       return state;
   }
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }, callback) => {
-    trackerApi
-      .post("/signup", { email, password })
-      .then((response) => console.log(response.data))
-      .catch((err) => {
-        dispatch({
-          type: "add_error",
-          payload: "Something went wrong with sign up",
-        });
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      const token = response.data.token;
+
+      await AsyncStorage.setItem("token", token);
+      dispatch({ type: "signup", payload: token });
+    } catch (err) {
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with sign up",
       });
+    }
 
     // make api request to sign up with that email and password
     // if we sign up, modify our state, and say that we are authenticated
@@ -46,5 +53,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { token: "", errorMessage: "" }
+  { token: null, errorMessage: "" }
 );
