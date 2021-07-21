32e1aee867a8d25a2addb6f75a20e8c7d54307dc
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-22 00:17:46 +0300

    feat: Add library for getting location and request permission for access to user location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-navigation";
-import Map from "../components/Map";
+import { requestForegroundPermissionsAsync } from "expo-location";
 import { Text } from "react-native-elements";
 
+import Map from "../components/Map";
+import { StyleSheet } from "react-native";
+
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching().then();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {err ? (
+        <Text style={styles.error}>Please enable location services</Text>
+      ) : null}
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  error: {
+    color: "red",
+    fontSize: 24,
+  },
+});
 
 export default TrackCreateScreen;
