55d88cd698efa3b05135409fa4325d481cadc032
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-24 12:39:03 +0300

    refactor: Extract the logic for getting the user's position into a custom hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
+
+export default (callback) => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        // throw new Error("Location permission not granted");
+        setErr("Location permission not granted");
+      }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        callback
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching().then();
+  }, []);
+
+  return [err];
+};
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,46 +1,16 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import { Text } from "react-native-elements";
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from "expo-location";
 
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
+import useLocation from "../hooks/useLocation";
 // import "../_mockLocation";
 
 const TrackCreateScreen = () => {
-  const [err, setErr] = useState(null);
   const { addLocation } = useContext(LocationContext);
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        // throw new Error("Location permission not granted");
-        setErr("Location permission not granted");
-      }
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => {
-          addLocation(location);
-        }
-      );
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching().then();
-  }, []);
+  const [err] = useLocation(addLocation);
 
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
