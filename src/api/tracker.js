815bbcec609535d073d62e6ea767203dd913d660
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-25 20:08:24 +0300

    feat: Add automatic authentication in middleware

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,23 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
-  baseURL: "https://b218db8a0b73.ngrok.io",
+const instance = axios.create({
+  baseURL: "https://8ff77bee9b43.ngrok.io",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
