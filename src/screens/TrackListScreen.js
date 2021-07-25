e8d83ad6d234f1189c54ce3bc0e50175bb05fbcd
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-07-25 21:00:08 +0300

    feat: Add display tracks fetched from API on track list screen

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { Text, Button } from "react-native";
+import React, { useContext } from "react";
+import { Text, FlatList, Pressable } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { Context as TrackContext } from "../context/TrackContext";
+import { ListItem } from "react-native-elements";
 
-const TrackListScreen = ({ navigation }) => {
+const TrackListScreen = () => {
+  const { state, fetchTracks } = useContext(TrackContext);
+
+  const keyExtractor = (item) => `id: ${item._id}`;
+
+  const renderItem = ({ item }) => {
+    return (
+      <Pressable>
+        <ListItem>
+          <ListItem.Content>
+            <ListItem.Title>{item.name}</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+      </Pressable>
+    );
+  };
+
   return (
     <>
+      <NavigationEvents onWillFocus={fetchTracks} />
       <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate("TrackDetail")}
+      <FlatList
+        data={state}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
       />
     </>
   );
